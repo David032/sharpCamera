86cd47e562d3b3b1258b124d9e901d12c8dee631
David K <davek032@outlook.com>
2023-09-11 16:11:13 +0100

    Inital Setup

---
+++ sharpCamera/PiCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+namespace sharpCamera
+{
+    public class PiCamera
+    {
+        private TaskCompletionSource<bool>? imageTaken;
+        private Process? cameraCommand;
+        private string outputPath = "/output";
+
+        public PiCamera()
+        {
+            if (!Directory.Exists(outputPath))
+            {
+                // Can't set permissions for unix in .net 6 easily :(
+                // DirectoryInfo di = Directory.CreateDirectory(outputPath);
+            }
+        }
+
+        public void TakePicture()
+        {
+            imageTaken = new TaskCompletionSource<bool>();
+            try
+            {
+                using (cameraCommand = new Process())
+                {
+                    string fileNameToOutput = "-o " + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg";
+                    cameraCommand.StartInfo.UseShellExecute = false;
+                    cameraCommand.StartInfo.FileName = "raspistill";
+                    cameraCommand.StartInfo.Arguments = fileNameToOutput;
+
+#if DEBUG
+                    Console.Out.WriteLine(cameraCommand.StartInfo.Arguments);
+#endif
+                    cameraCommand.Start();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ sharpCameraExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using sharpCamera;
+
+namespace sharpCameraExample
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Say cheese!");
+            PiCamera cam = new PiCamera();
+            cam.TakePicture();
+        }
+    }
+}
